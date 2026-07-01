fd5a937cac683af3e9bd5a0605f91ab1326ea589
durginiveditha <durginiveditha@gmail.com>
2026-07-01 23:08:23 +0530

    Add AuditLog entity

--- src/main/java/com/incture/erasm/entity/AuditLog.java
+++ src/main/java/com/incture/erasm/entity/AuditLog.java
@@ -1,5 +1,93 @@
 package com.incture.erasm.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "audit_logs")
 public class AuditLog {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long auditId;
+
+    @Column(nullable = false)
+    private String action;
+
+    @Column(nullable = false)
+    private String createdBy;
+
+    @Column(nullable = false)
+    private String modifiedBy;
+
+    @Column(nullable = false)
+    private String createdDate;
+
+    @Column(nullable = false)
+    private String modifiedDate;
+
+    // Default Constructor
+    public AuditLog() {
+    }
+
+    // Parameterized Constructor
+    public AuditLog(Long auditId, String action,
+                    String createdBy, String modifiedBy,
+                    String createdDate, String modifiedDate) {
+        this.auditId = auditId;
+        this.action = action;
+        this.createdBy = createdBy;
+        this.modifiedBy = modifiedBy;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
+    // Getters and Setters
+
+    public Long getAuditId() {
+        return auditId;
+    }
+
+    public void setAuditId(Long auditId) {
+        this.auditId = auditId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
