179f45fe7a8031adcf0be60cd1817a56dfb90179
durginiveditha <durginiveditha@gmail.com>
2026-07-01 22:45:20 +0530

    Add EmployeeSkill entity

--- src/main/java/com/incture/erasm/entity/EmployeeSkill.java
+++ src/main/java/com/incture/erasm/entity/EmployeeSkill.java
@@ -1,5 +1,82 @@
 package com.incture.erasm.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee_skills")
 public class EmployeeSkill {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeSkillId;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_id", nullable = false)
+    private Skill skill;
+
+    @Column(nullable = false)
+    private String skillLevel;
+
+    @Column(nullable = false)
+    private int experienceYears;
+
+    // Default Constructor
+    public EmployeeSkill() {
+    }
+
+    // Parameterized Constructor
+    public EmployeeSkill(Long employeeSkillId, Employee employee,
+                         Skill skill, String skillLevel, int experienceYears) {
+        this.employeeSkillId = employeeSkillId;
+        this.employee = employee;
+        this.skill = skill;
+        this.skillLevel = skillLevel;
+        this.experienceYears = experienceYears;
+    }
+
+    // Getters and Setters
+
+    public Long getEmployeeSkillId() {
+        return employeeSkillId;
+    }
+
+    public void setEmployeeSkillId(Long employeeSkillId) {
+        this.employeeSkillId = employeeSkillId;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+
+    public String getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(String skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
 }
